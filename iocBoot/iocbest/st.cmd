b351d2b0c53c7696a8cb60db5ac54b7d5d510b58
Jan Marjanovic <j.marjanovic@caenels.com>
2014-10-22 12:29:28 +0200

    Created with makeBaseApp

---
+++ iocBoot/iocbest/st.cmd
@@ -0,0 +1,18 @@
+#!../../bin/linux-x86_64/best
+
+## You may have to change best to something else
+## everywhere it appears in this file
+
+#< envPaths
+
+## Register all support components
+dbLoadDatabase("../../dbd/best.dbd",0,0)
+best_registerRecordDeviceDriver(pdbbase) 
+
+## Load record instances
+dbLoadRecords("../../db/best.db","user=jan")
+
+iocInit()
+
+## Start any sequence programs
+#seq sncbest,"user=jan"
